ee7dc7d4250a046215664d48c04f541dccbd6b5e
AntarcBall <78745248+AntarcBall@users.noreply.github.com>
2021-06-08 14:17:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 box = document.getElementById('display')
-box.innerHTML = '혼자만의채팅 ㄱ'
+box.innerHTML = '윤서진 님이 입장하셨습니다. 그를 자극하지 마세요.'
 keys = {};
 document.addEventListener('keydown', e => {keys[e.key] = true})  //키 체크중 
 document.addEventListener('keyup', e => {if(keys[e.key]) delete keys[e.key]})  
 input = document.getElementById('text')
+hishit = ['(대충이모티콘)','ㄷㄷ','ㄷㄷ','우흥','아따']
 function writeNewRow(value) {
     box.innerHTML += '<br/>' + value
 }
@@ -12,6 +13,7 @@ btn = document.getElementById('btn');
 function write(){
     if (input.value){
     writeNewRow('사용자: '+input.value);
+    writeNewRow('윤서진: '+hishit[Math.floor(Math.random()*3)]+' . . . . . . . .');
     input.value = '';}
 }
 btn.addEventListener('click',function () {
